839454a5d28b07ab4feb6de259716d5502f32fc0
Georgi.Petrov <georgi.petrov@entaincorp.com>
2023-04-03 16:47:05 +0300

    Add project files.

---
+++ DataAccessLibrary/SampleDataAccess.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public class SampleDataAccess
+    {
+        public List<EmployeeModel> GetEmployees() { 
+            List<EmployeeModel> output = new List<EmployeeModel>();
+
+            output.Add(new EmployeeModel() { FirstName = "Tim", LastName = "Corey"});
+            output.Add(new EmployeeModel() { FirstName = "Sue", LastName = "Storm"});
+            output.Add(new EmployeeModel() { FirstName = "Jane", LastName = "Jones"});
+
+            // delay the output returning for 3 sec
+            Thread.Sleep(3000);
+            return output;
+        }
+        public async Task<List<EmployeeModel>> GetEmployeesAsync()
+        {
+            List<EmployeeModel> output = new List<EmployeeModel>();
+
+            output.Add(new EmployeeModel() { FirstName = "Tim", LastName = "Corey" });
+            output.Add(new EmployeeModel() { FirstName = "Sue", LastName = "Storm" });
+            output.Add(new EmployeeModel() { FirstName = "Jane", LastName = "Jones" });
+
+            // turn it into async call which is waiting
+            await Task.Delay(3000);
+            return output;
+        }
+    }
+}
