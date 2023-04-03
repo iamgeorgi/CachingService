aa0a7a7c3a6bfe6d7fb48fa6aa286a815a02062d
Georgi.Petrov <georgi.petrov@entaincorp.com>
2023-04-03 17:19:40 +0300

    Add InMemoryCache service

--- DataAccessLibrary/SampleDataAccess.cs
+++ DataAccessLibrary/SampleDataAccess.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Caching.Memory;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,12 @@ namespace DataAccessLibrary
 {
     public class SampleDataAccess
     {
+        private readonly IMemoryCache _memoryCache;
+
+        public SampleDataAccess(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
         public List<EmployeeModel> GetEmployees() { 
             List<EmployeeModel> output = new List<EmployeeModel>();
 
@@ -29,6 +36,28 @@ namespace DataAccessLibrary
 
             // turn it into async call which is waiting
             await Task.Delay(3000);
+            return output;
+        }
+
+        public async Task<List<EmployeeModel>> GetEmployeesCache()
+        {
+            List<EmployeeModel> output;
+
+            output = _memoryCache.Get<List<EmployeeModel>>("employees");
+
+            if (output == null)
+            {
+                output = new List<EmployeeModel>();
+                output.Add(new EmployeeModel() { FirstName = "Tim", LastName = "Corey" });
+                output.Add(new EmployeeModel() { FirstName = "Sue", LastName = "Storm" });
+                output.Add(new EmployeeModel() { FirstName = "Jane", LastName = "Jones" });
+
+                await Task.Delay(3000);
+
+                // it will be stored in memory for 1 minute
+                _memoryCache.Set("employees", output, TimeSpan.FromMinutes(1));
+            }
+
             return output;
         }
     }
